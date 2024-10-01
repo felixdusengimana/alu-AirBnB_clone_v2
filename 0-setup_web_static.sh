f8bf8c2c5bebf937d9ae1ea934ccd6041bc3ecdc
felixdusengimana <phelixdusengimana@gmail.com>
2024-10-01 11:50:06 +0200

    use restart instead of start

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,4 +9,4 @@ echo "<html><head></head><body>Holberton School, Felix's test</body></html>" | s
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo service nginx restart
